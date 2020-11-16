c8159c7343fdd7d3c95222d682cc3cabe18be24b
Daniel Patterson <danielanywhere@users.noreply.github.com>
2020-11-16 10:53:02 -0700

    Anatomy of a Decision Node

---
+++ Info/AnatomyOfADecisionNode.md
@@ -0,0 +1,22 @@
+Anatomy of a Decision Node
+==========================
+
+Last updated by Daniel Patterson on November 16, 2020
+
+Decision nodes are useful in building any of the following communication
+scenarios.
+
+-   Chatbot-based micro-learning.
+
+-   Conversational training.
+
+-   Just-in-time learning.
+
+-   Prompted dialog.
+
+-   Scenario-based training.
+
+The following illustration describes the parts of a single decision node
+and highlights how those parts are mapped to chatbot output.
+
+<img src="../Images/AnatomyOfADecisionNode.png" style="width:6.5in;height:3.65625in" alt="Diagram Description automatically generated" />
---
+++ Scripts/ConvertAnatomyOfADecisionNodeToMarkdown.cmd
@@ -0,0 +1,18 @@
+:: ConvertAnatomyOfADecisionNodeToMarkdown.cmd
+:: Convert the document AnatomyOfADecisionNode.docx to Info/AnatomyOfADecisionNode.md
+:: Dependencies:
+::  pandoc (https://github.com/jgm/pandoc)
+::  FindAndReplace (https://github.com/danielanywhere/FindAndReplace)
+::
+:: Copyright (c). 2020 Daniel Patterson, MCSD (danielanywhere)
+:: Copyright (c). 2020 Ascendant Design and Training, LLC
+:: Released for public access under the MIT License.
+:: https://opensource.org/licenses/MIT
+@ECHO OFF
+SET SOURCE=%USERPROFILE%\Documents\GitHub\Scaffold\Docs\AnatomyOfADecisionNode.docx
+SET TARGET=%USERPROFILE%\Documents\GitHub\Scaffold\Info\AnatomyOfADecisionNode.md
+SET POSTCONFIG=%USERPROFILE%\Documents\GitHub\Scaffold\Scripts\AnatomyOfADecisionNodeDocPostConversion.json
+SET FAR=C:\Scripts\FindAndReplace\FindAndReplace.exe
+
+PANDOC -f docx -t markdown_strict+pipe_tables -s "%SOURCE%" -o "%TARGET%"
+"%FAR%" /files:"%TARGET%" /patternfile:"%POSTCONFIG%"
